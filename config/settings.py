300f72ff2f58769363cc461b23f000332a1cd768
Shivansh Shukla <shivanshnshukla@gmail.com>
2025-12-04 17:52:11 -0500

    changed from 2.0 flash to 2.5 flash model

--- config/settings.py
+++ config/settings.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 # Gemini Configuration
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
-GEMINI_MODEL_NAME = os.getenv("GEMINI_MODEL_NAME", "gemini-2.0-flash")
+GEMINI_MODEL_NAME = os.getenv("GEMINI_MODEL_NAME", "gemini-2.5-flash")
 
 # App Configuration
 APP_NAME = "SpendLens" #"Emotion-Aware Money Storybook"
